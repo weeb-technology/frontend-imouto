f0fb9b167e6642ea2740aaf1f836fafeba08d711
getynge <sparklytarnish+git@protonmail.com>
2019-04-28 01:42:19 -0400

    Added notfound page and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import {AppComponent} from './app.component';
+import {MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginModule} from './login/login.module';
+import {LoginComponent} from './login/login.component';
+import {NotfoundModule} from './notfound/notfound.module';
+import {NotfoundComponent} from './notfound/notfound.component';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    LoginModule,
+    NotfoundModule,
+
+    RouterModule.forRoot(
+      routes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
